32da0e39c984f3f72e7d1c47873d89174d8d1d44
fadhil <fadhildarma13@gmail.com>
2021-08-26 17:43:53 +0700

    Install ReactJS in Folder client and install ExpressJS in Folder server

---
+++ client/src/AppProject.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import bootstrap components
+import { Container } from "react-bootstrap";
+
+export default function AppProject() {
+  return (
+    <div>
+      <Container>
+        <h1>This is AppProject</h1>
+      </Container>
+    </div>
+  );
+}
